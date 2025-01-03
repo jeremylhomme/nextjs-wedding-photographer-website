d300ca46abe9168023a1cd40aebd0bdeeddd406e
Ylwbro <jeremydan.lhomme@gmail.com>
2025-01-03 15:15:50 +0100

    remove sizes

--- src/components/photo-tiles.tsx
+++ src/components/photo-tiles.tsx
@@ -51,7 +51,6 @@ export function PhotoTiles({ images }: PhotoTilesProps) {
             onImageLoad={path =>
               setLoadedImages(prev => ({ ...prev, [path]: true }))
             }
-            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
           />
         </div>
       ))}
